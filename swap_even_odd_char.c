0fe7d0a013c2312d9e71e8589d24734f9fa24ca0
Yahiya Sharief <sysharief@gmail.com>
2016-03-28 20:40:14 +0530

    Update swap_even_odd_char.c

--- swap_even_odd_char.c
+++ swap_even_odd_char.c
@@ -3,21 +3,23 @@
 #include<stdlib.h>
 void main()
 {
-	char *a,temp,temp2;
-	int i;
-	a=malloc(1000000);
-	scanf("%s",a);
-	i=0;
-	if(strlen(a)%2!=0){
-	temp2=*(a+(strlen(a)-1));
-	}
-	while(i<strlen(a)){
-		temp=*(a+i);
-		*(a+i)=*(a+(i+1));
-		*(a+(i+1))=temp;
-		i=i+2;
-	}
-	*(a+(strlen(a)-1))=temp2;
-	printf("%s",a);
+char *a,temp,temp2;
+int i;
+a=malloc(1000000);
+scanf("%s",a);
+i=0;
+if(strlen(a)%2!=0)
+{
+temp2=*(a+(strlen(a)-1));
+}
+while(i<strlen(a))
+{
+temp=*(a+i);
+*(a+i)=*(a+(i+1));
+*(a+(i+1))=temp;
+i=i+2;
+}
+*(a+(strlen(a)-1))=temp2;
+printf("%s",a);
 	
 }
